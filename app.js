fa303b83e6e74ee2e0b86d7484f67d62281065da
almeidaribeiro <almeidaribeiro.b@gmail.com>
2019-07-05 17:29:38 -0300

    routes user and products

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const db = require('./models/index');
 
-const PORT = process.env.PORT || 4566;
+const PORT = process.env.PORT || 4557;
 app.listen(PORT, console.log(`server started on port ${PORT}`));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/users", require('./routes/user'));
 app.use("/products", require('./routes/products'));
-app.use("/order", require('./routes/order'));
+//app.use("/orders", require('./routes/order'));
 
 db.sequelize.sync();
